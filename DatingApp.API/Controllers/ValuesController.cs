6419761e3160d03a2924e58ea1e42acfcd787ad4
jadarya <jadarya@mail.ru>
2019-03-30 13:43:23 +0900

    End of secion 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,41 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DatingApp.API.Data;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [ApiController]
+    public class ValuesController : ControllerBase
     {
-        private readonly DataContext _context;
-        public ValuesController(DataContext context) => _context = context;
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public ActionResult<IEnumerable<string>> Get()
         {
-            var values = _context.Values.ToList();
-
-            return Ok(values);
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public ActionResult<string> Get(int id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == id);
-            return Ok(value);
+            return "value";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
-        {}
+        public ActionResult<int> Post()
+        {
+            return 1;
+        }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody] string value)
         {
         }
 
