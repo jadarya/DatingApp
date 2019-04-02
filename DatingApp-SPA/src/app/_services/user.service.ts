bacf020c0e1969afbe997b8ca8102e37315458c4
jadarya <jadarya@mail.ru>
2019-04-02 21:44:38 +0900

    End of section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+// remove because we added tokenGetter
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     // This space after 'Bearer ' is very important else word merges with token
+//     Authorization: 'Bearer ' + localStorage.getItem('token')
+//   })
+// };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
+
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
+}
